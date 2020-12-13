06a8f9215aa417541195c65ab600a0d425eee013
Fauzan Affan Zaki <fauzan.zaki@jatis.com>
2020-12-13 12:21:44 +0700

    update

--- 2-Callback/index.js
+++ 2-Callback/index.js
@@ -9,12 +9,12 @@ const myCallback = () => {
   console.log("Hello Callback");
 }
 
-// main(1, 2, myCallback);
+main(1, 2, myCallback);
 
 /*
 Output :
-    1 2
-    hello callback
+  1 2
+  hello callback
 */
 
 // Synchronous Callback
@@ -61,3 +61,118 @@ where the result of the process requires a certain time interval to produce
 output. We will find asynchronous processes on Ajax processes, 
 HTTP communications, file operations, timers, etc.
 */
+
+// Pada synchronous output di prosess berdasarkan urutan kode.
+
+// function p1() {
+//   console.log('p1 done')
+// }
+// function p2() {
+//   console.log('p2 done')
+// }
+// function p3() {
+//   console.log('p3 done')
+// }
+// p1()
+// p2()
+// p3()
+
+/* Output :
+p1 done
+p2 done
+p3 done
+*/
+
+/* Tetapi pada proses asynchronous output dari kode yang dituliskan 
+tidak selalu berurutan. Hasilnya tergantung yang mana 
+yang lebih dulu selesai. Perhatikan contoh berikut:
+*/
+
+// function p1() {
+//   console.log('p1 done')
+// }
+// function p2() {
+//   //setTimeout or delay for asynchronous simulation 
+//   setTimeout(
+//     function() {
+//       console.log('p2 done')
+//     },100
+//   )
+// }
+// function p3() {
+//   console.log('p3 done')
+// }
+// p1()
+// p2()
+// p3()
+
+/* Output :
+p1 done
+p3 done
+p2 done
+*/
+
+// Solusinya adalah dengan membuat p3 menjadi callback bagi p2.
+
+// function p1() {
+//   console.log('p1 done')
+//  }
+ 
+//  function p2(callback) {
+//   setTimeout(
+//    function() {
+//     console.log('p2 done')
+//      callback()
+//    },100
+//    )
+//  }
+ 
+//  function p3() {
+//    console.log('p3 done')
+//  }
+
+//  p1()
+//  p2(p3)
+
+// - Callback in ajax request
+
+// Problem
+
+// data=requestAjax() // asynchronous process
+// showResult(data) //undefined
+
+// Solution
+
+// function requestAjax(callback){
+
+//   // inisialisasi library XML Http Request
+//   var xhr = new XMLHttpRequest();
+
+//   // set target request
+//   xhr.open('GET','https://jsonplaceholder.typicode.com/users/1')
+
+//   // terapkan callback
+//   xhr.onload = () => {
+//     if(xhr.status === 200){
+//       callback(xhr.response)
+//     }else{
+//       callback('Failed')
+//     }
+//   }
+
+//   // mulai request
+//   xhr.send()
+  
+// }
+
+// function showResult(data){
+//   if (data != 'Failed'){
+//     //tampilkan Data
+//     data=JSON.parse(data)
+//     console.log(data)
+//   } else {
+//     console.log('Error bosQ')
+//   }
+// }
+
+// requestAjax(showResult)
